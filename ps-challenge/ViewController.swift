5580bac232aa4b4d429275b32532882a1067271d
jiggybear <cagri.sahan@gmail.com>
2018-03-20 23:19:25 -0500

    Can collapse hamburger menu. Will work on gesture recognizer.

--- ps-challenge/ViewController.swift
+++ ps-challenge/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class mapViewController: UIViewController {
+    //MARK: IBOutlets
+    
+    @IBOutlet weak var hamburgerMenuView: UIView!
+    @IBOutlet weak var hamburgerMenuLeadingEdge: NSLayoutConstraint!
+    
+    @IBAction func hamburgerMenuButtonPressed(_ sender: Any) {
+        moveHamburgerMenu()
+    }
+    
+    var menuShouldBeOpen = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        hamburgerMenuLeadingEdge.constant = -hamburgerMenuView.frame.width
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func moveHamburgerMenu() {
+        hamburgerMenuLeadingEdge.constant = menuShouldBeOpen ? 0 : -hamburgerMenuView.frame.width
+        UIView.animate(withDuration: 0.2, delay: 0.0, options: .curveEaseIn, animations: {
+            self.view.layoutIfNeeded()
+        }) { [unowned self] animateCompleted in
+            if animateCompleted {
+                self.menuShouldBeOpen = !self.menuShouldBeOpen
+            }
+        }
+    }
 }
 
